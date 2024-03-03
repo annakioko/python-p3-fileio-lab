c44d3921edb2279765618f4598515c9521423ffb
Anna Kioko <annakioko00@gmail.com>
2024-03-03 07:58:45 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    file_name=f"{file_name}.txt"
+    with open(file_name, mode='w') as f:
+        f.write(file_content)
+    
 
 def append_file(file_name, append_content):
-    pass
+    file_name = f"{file_name}.txt"
+    with open(file_name, mode="a") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name =f"{file_name}.txt"
+    with open(file_name, mode="r") as f:
+        return f.read()
